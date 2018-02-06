e30247d8e29466dff91c8c472732eee711c89c76
CanftIn <wwc7033@gmail.com>
2018-02-06 23:50:30 +0800

    update

---
+++ Calc/Calc/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Calc
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOp = new System.Windows.Forms.Button();
+            this.InputExp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOp
+            // 
+            this.btnOp.Location = new System.Drawing.Point(63, 64);
+            this.btnOp.Name = "btnOp";
+            this.btnOp.Size = new System.Drawing.Size(120, 23);
+            this.btnOp.TabIndex = 1;
+            this.btnOp.Text = "运算";
+            this.btnOp.UseVisualStyleBackColor = true;
+            this.btnOp.Click += new System.EventHandler(this.btnOp_Click);
+            // 
+            // InputExp
+            // 
+            this.InputExp.Location = new System.Drawing.Point(63, 28);
+            this.InputExp.Name = "InputExp";
+            this.InputExp.Size = new System.Drawing.Size(120, 20);
+            this.InputExp.TabIndex = 2;
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 130);
+            this.Controls.Add(this.InputExp);
+            this.Controls.Add(this.btnOp);
+            this.Name = "Calc";
+            this.Text = "Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Button btnOp;
+        private System.Windows.Forms.TextBox InputExp;
+    }
+}
+
---
+++ Calc/Calc/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOp_Click(object sender, EventArgs e)
+        {
+            string str = InputExp.Text;
+            int result = 0;
+            while(true)
+            {
+                if(calc(ref str, ref result))
+                {
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+        }
+
+        public int getExpLength()
+        {
+            string str = InputExp.Text;
+            int length = str.Length;
+            return length - 1;
+        }
+
+        char getStrIndexOf(int i)
+        {
+            string str = InputExp.Text;
+            return str[i];
+        }
+
+        public char test_char(ref int i, int end, string candidates)
+        {
+            if (i == end) return (char)0;
+            var result = candidates.IndexOf(getStrIndexOf(i));
+            if (result == 0) return (char)0;
+            i++;
+            return getStrIndexOf(i);
+        }
+
+        bool number(ref int i, int end, ref int result)
+        {
+            result = 0;
+            int j = i;
+            while (j != end && '0' <= getStrIndexOf(j) && getStrIndexOf(j) <= '9')
+            {
+                result = result * 10 + (getStrIndexOf(j) - '0');
+                j++;
+            }
+            if (i == j) return false;
+            i = j;
+            return true;
+        }
+
+        bool factor(ref int i, int end, ref int result)
+        {
+            if (number(ref i, end, ref result)) return true;
+            var j = i;
+            if (test_char(ref j, end, "(") == 0) return false;
+            if (!exp(ref j, end, ref result)) return false;
+            if (test_char(ref j, end, ")") == 0) return false;
+            i = j;
+            return true;
+        }
+
+        public bool term(ref int i, int end, ref int result)
+        {
+            if (!factor(ref i, end, ref result)) return false;
+            while(true)
+            {
+                var j = i;
+                var op = test_char(ref j, end, "*/");
+                if (op != '\0')
+                {
+                    int next = 0;
+                    if (!factor(ref j, end, ref next)) return true;
+                    result = op == '*'
+                        ? result * next
+                        : result / next;
+                    i = j;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public bool exp(ref int i, int end, ref int result)
+        {
+            if (!term(ref i, end, ref result)) return false;
+            while(true)
+            {
+                var j = i;
+                var op = test_char(ref j, end, "+-");
+                if (op != '\0')
+                {
+                    int next = 0;
+                    if (!factor(ref j, end, ref next)) return true;
+                    result = op == '+'
+                        ? result + next
+                        : result - next;
+                    i = j;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public bool calc(ref string str, ref int result)
+        {
+            int begin = 0;
+            int end = str.Length - 1;
+            return exp(ref begin, end, ref result);
+        }
+    }
+}
